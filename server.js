f05b246b15a9527ef793baa8a4f9a5a3021f4006
saa79 <sindhuri.addada@sky-uk>
2020-04-09 12:51:39 +0100

    nthg

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 9000
 http
     .createServer((req, res) => {
         console.log('New connection')
-        res.end('Hello Cloud Run v2 ' + new Date())
+        res.end('Hello Cloud Run v3 ' + new Date())
     })
     .listen(PORT, () => console.log('Listening on', PORT))
